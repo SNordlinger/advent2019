1c6a04f96d4e66995b8611bfa4d4381ce1495a89
Sam Nordlinger <snordlinger@gmail.com>
2019-12-01 23:23:06 -0700

    Make tests type independent

--- 1/clj/test/fuel_test.clj
+++ 1/clj/test/fuel_test.clj
@@ -2,12 +2,12 @@
                         [fuel :refer [fuel-req total-req]]))
 
 (deftest part1
-  (is (= (fuel-req 12) 2.0))
-  (is (= (fuel-req 14) 2.0))
-  (is (= (fuel-req 1969) 654.0))
-  (is (= (fuel-req 100756) 33583.0)))
+  (is (== (fuel-req 12) 2))
+  (is (== (fuel-req 14) 2))
+  (is (== (fuel-req 1969) 654))
+  (is (== (fuel-req 100756) 33583)))
 
 (deftest part2
-  (is (= (total-req 14) 2.0))
-  (is (= (total-req 1969) 966.0))
-  (is (= (total-req 100756) 50346.0)))
+  (is (== (total-req 14) 2))
+  (is (== (total-req 1969) 966))
+  (is (== (total-req 100756) 50346)))
