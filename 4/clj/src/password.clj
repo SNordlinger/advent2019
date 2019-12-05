52f6e56636f122c74115cdcc12e45c132243dcd4
Sam Nordlinger <snordlinger@gmail.com>
2019-12-05 16:47:53 -0700

    Fix day 4 function names

--- 4/clj/src/password.clj
+++ 4/clj/src/password.clj
@@ -6,25 +6,25 @@
 (defn is-incresing? [digits]
   (= digits (sort digits)))
 
-(defn is-repeating? [digits]
+(defn is-repeating [digits]
   (some true? (map = digits (cons nil digits))))
 
-(defn double-repeat? [digits]
+(defn double-repeat [digits]
   (some (fn [[_ freq]] (= freq 2)) (frequencies digits)))
 
-(defn number-valid-part1? [number]
+(defn number-valid-part1 [number]
   (let [digits (split-digits number)]
-    (and (is-incresing? digits) (is-repeating? digits))))
+    (and (is-incresing? digits) (is-repeating digits))))
 
-(defn number-valid-part2? [number]
+(defn number-valid-part2 [number]
   (let [digits (split-digits number)]
-    (and (is-incresing? digits) (double-repeat? digits))))
+    (and (is-incresing? digits) (double-repeat digits))))
 
 (defn part1 []
-  (count (filter number-valid-part1? (range 125730 579381))))
+  (count (filter number-valid-part1 (range 125730 579381))))
 
 (defn part2 []
-  (count (filter number-valid-part2? (range 125730 579381))))
+  (count (filter number-valid-part2 (range 125730 579381))))
 
 (defn -main []
   (println (str "Part 1: " (part1)))
